7f67a6237858db1a8f974eca9f6f1dcaa7fcf070
RodrigoIsac0505 <87947745+RodrigoIsac0505@users.noreply.github.com>
2021-07-25 14:27:20 -0500

    Update ProcesosProfesores.php

--- comun/ProcesosProfesores.php
+++ comun/ProcesosProfesores.php
@@ -1,2 +1,21 @@
-<?php 
-//ARCHIVOS PARA ESTABLECER PROCESOS SOBRE LA BD EN TBALAS DE PROFESORES
+<?php
+
+    class ProcesosJuego{
+
+        function AllPreguntas(){
+            $con = new Conexion();
+
+            $allPreguntas = mysqli_query($con->Conectar(),"SELECT * FROM scru_pregunta");
+            $countRows = mysqli_num_rows($allPreguntas);
+                if($countRows>0){
+                    $datos = array();
+                    while($row = mysqli_fetch_assoc($allPreguntas)){
+                        $datos[]=$row;
+                    }
+                    return $datos;
+                }
+                return [1];
+        }
+        
+    }
+?>
