d52897252f81298e7ab3cd9ddd61aab92fa7ae89
RodrigoIsac0505 <87947745+RodrigoIsac0505@users.noreply.github.com>
2021-07-25 14:24:43 -0500

    Update conexion.php

--- comun/conexion.php
+++ comun/conexion.php
@@ -1,2 +1,13 @@
-<?php 
-//ARCHIVOS PARA ESTABLECER CONEXIONES A LA BD
+<?php
+   DEFINE("DB_HOST","mariadb-17648-0.cloudclusters.net");
+   DEFINE("DB_USER","fisc");
+   DEFINE("DB_PASS","fisc2021*");
+   DEFINE("DB_PORT","17664");
+   DEFINE("DB_NAME","fisc_games");
+
+    class Conexion{
+        function Conectar(){
+            return mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME,DB_PORT);
+        }
+    }    
+?>
